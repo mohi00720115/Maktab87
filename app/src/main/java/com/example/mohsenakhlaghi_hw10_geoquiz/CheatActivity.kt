9a86a1f36919bb4081d434c0aee912c74c90f349
MohsenAkhlaghi <mohi00720115@gmail.com>
2023-02-16 00:23:44 +0330

    GeoQuiz

---
+++ app/src/main/java/com/example/mohsenakhlaghi_hw10_geoquiz/CheatActivity.kt
@@ -0,0 +1,33 @@
+package com.example.mohsenakhlaghi_hw10_geoquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class CheatActivity : AppCompatActivity() {
+
+
+    private var obj = GeoQuiz
+    private lateinit var btnCheatActivity: Button
+    private lateinit var tvCheat: TextView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cheat)
+
+        btnCheatActivity = findViewById<Button>(R.id.btn_cheatActivity)
+        tvCheat = findViewById<Button>(R.id.tv_cheat)
+
+        val answer = intent.getBooleanExtra("answer", false).toString()
+
+        btnCheatActivity.setOnClickListener {
+            tvCheat.text = answer
+            obj.cheatStatus = true
+        }
+
+    }
+
+
+}
